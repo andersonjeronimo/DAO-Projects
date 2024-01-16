c75726e90d3cb8db4e34c8d7ec3a064b1fac1c03
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2024-01-15 23:25:12 -0300

    Module 3, lesson 5, topics 1-8 : complete

--- condominium/contracts/Condominium.sol
+++ condominium/contracts/Condominium.sol
@@ -11,33 +11,134 @@ contract Condominium {
     mapping(address => uint16) public residents; //wallet -> ex.:"1101"
     mapping(uint16 => bool) public residences; //cadastro de todas as unidades
 
-    string public message;
+    enum Status {
+        IDLE,
+        VOTING,
+        APPROVED,
+        DENIED
+    }
+
+    struct Topic {
+        string title;
+        string description;
+        Status status;
+        uint256 createdDate;
+        uint256 startDate;
+        uint256 endDate;
+    }
+
+    mapping(bytes32 => Topic) public topics;
 
     constructor() {
         //owner = payable(msg.sender);
         manager = msg.sender;
-        message = "Hello World";
         for (uint8 i = 1; i <= 2; i++) {
             //blocos
             for (uint8 j = 1; j <= 5; j++) {
                 //andares
                 for (uint8 k = 1; k <= 4; k++) {
                     //unidades
-                    residences[(i * 1000) + (j * 100) + k] = true;
+                    unchecked {
+                        residences[(i * 1000) + (j * 100) + k] = true;
+                    }
                 }
             }
-        }//END_OF_LOOP
+        }
     }
 
-    function append(
-        string memory a,
-        string memory b
-    ) internal pure returns (string memory) {
-        return string(abi.encodePacked(a, b));
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager is authorized");
+        _;
     }
 
-    function setMessage(string memory newMessage) public {
-        require(msg.sender == manager, "You do not have permission");
-        message = newMessage;
+    modifier onlyCouncil() {
+        require(
+            msg.sender == manager || counselors[msg.sender],
+            "Only manager or council is authorized"
+        );
+        _;
     }
+
+    modifier onlyResidents() {
+        require(
+            msg.sender == manager || isResident(msg.sender),
+            "Only manager or resident is authorized"
+        );
+        _;
+    }
+
+    function isResident(address resident) public view returns (bool) {
+        return residents[resident] > 0;
+    }
+
+    function residenceExists(uint16 residenceId) public view returns (bool) {
+        return residences[residenceId];
+    }
+
+    function addResident(
+        address resident,
+        uint16 residenceId
+    ) external onlyCouncil {
+        require(residenceExists(residenceId), "This residence does not exists");
+        residents[resident] = residenceId;
+    }
+
+    function removeResident(address resident) external onlyManager {
+        require(!counselors[resident], "A councelor cannot be removed");
+        delete residents[resident];
+        if (counselors[resident]) {
+            delete counselors[resident];
+        }
+    }
+
+    function setCouncelor(
+        address resident,
+        bool isEntering
+    ) external onlyManager {
+        if (isEntering) {
+            require(isResident(resident), "The councelor must be a resident");
+            counselors[resident] = true;
+        } else {
+            delete counselors[resident];
+        }
+    }
+
+    function setManager(address newManager) external onlyManager {
+        require(newManager != address(0), "The address must be valid");
+        manager = newManager;
+    }
+
+    function getTopic(string memory title) public view returns (Topic memory) {
+        bytes32 topicId = keccak256(bytes(title));
+        return topics[topicId];
+    }
+
+    function topicExists(string memory title) public view returns (bool) {
+        return getTopic(title).createdDate > 0;
+    }
+
+    function addTopic(
+        string memory title,
+        string memory description
+    ) external onlyResidents {
+        require(!topicExists(title), "Topic already exists");
+        Topic memory newTopic = Topic({
+            title: title,
+            description: description,
+            createdDate: block.timestamp,
+            startDate: 0,
+            endDate: 0,
+            status: Status.IDLE
+        });
+        topics[keccak256(bytes(title))] = newTopic;
+    }
+
+    function removeTopic(string memory title) external onlyManager {
+        Topic memory topic = getTopic(title);
+        require(topic.createdDate > 0, "Topic does not exists");
+        require(topic.status == Status.IDLE, "Only IDLE topics can be removed");
+        delete topics[keccak256(bytes(title))];
+    }
+
+
 }
--- condominium/test/Condominium.test.ts
+++ condominium/test/Condominium.test.ts
@@ -8,33 +8,75 @@ describe("Condominium", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [manager, resident] = await ethers.getSigners();
 
     const Condominium = await ethers.getContractFactory("Condominium");
-    const condominium = await Condominium.deploy();
+    const contract = await Condominium.deploy();
 
-    return { condominium, owner, otherAccount };
+    return { contract, manager, resident };
   }
 
   describe("Condominium tests", function () {
 
-    it("Should get message from the contract", async function () {
-      const { condominium, owner, otherAccount } = await loadFixture(deployFixture);
-      expect(await condominium.message()).to.equal("Hello World");
+    it("Should be residence", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      expect(await contract.residenceExists(1101)).to.equal(true);
+    }); 
+    
+    it("Should add resident", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      await contract.addResident(resident.address, 2102);
+      expect(await contract.isResident(resident.address)).to.equal(true);
     });
 
-    it("Should set message from the contract", async function () {
-      const { condominium, owner, otherAccount } = await loadFixture(deployFixture);
-      await condominium.setMessage("New Message");
-      expect(await condominium.message()).to.equal("New Message");
+    it("Should remove resident", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      await contract.addResident(resident.address, 2102);
+      await contract.removeResident(resident.address);      
+      expect(await contract.isResident(resident)).to.be.equal(false);
     });
 
-    it("Should NOT set message from the contract (permission)", async function () {
-      const { condominium, owner, otherAccount } = await loadFixture(deployFixture);
-      const instance = condominium.connect(otherAccount);      
-      await expect(instance.setMessage("Any Message")).to.be.revertedWith("You do not have permission");
+    it("Should NOT remove resident(permission)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      await contract.addResident(resident.address, 2102);
+      const instance = contract.connect(resident);      
+      await expect(instance.removeResident(resident.address)).to.revertedWith("Only manager is authorized");
+    });
+    
+    it("Should NOT remove resident(is councelor)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      await contract.addResident(resident.address, 2102);
+      await contract.setCouncelor(resident.address, true);      
+      await expect(contract.removeResident(resident.address)).to.revertedWith("A councelor cannot be removed");
+    });
+    
+    it("Should NOT add resident(residence does not exists)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);      
+      await expect(contract.addResident(resident.address, 1)).to.revertedWith("This residence does not exists");
     });
 
+    it("Should NOT add resident(permission)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      const instance = contract.connect(resident);
+      await expect(instance.addResident(resident.address, 1)).to.revertedWith("Only manager or council is authorized");
+    });
+
+    it("Should set manager", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      await contract.setManager(resident.address);      
+      expect(await contract.manager()).to.be.equal(resident.address);
+    });
+
+    it("Should NOT set manager(permission)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);
+      const instance = contract.connect(resident);
+      await expect(instance.setManager(resident.address)).to.revertedWith("Only manager is authorized");
+    });
+
+    it("Should NOT set manager(address)", async function () {
+      const { contract, manager, resident } = await loadFixture(deployFixture);      
+      await expect(contract.setManager(ethers.ZeroAddress)).to.revertedWith("The address must be valid");
+    });
 
 
   });
